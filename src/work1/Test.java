8506afc0d4e5532f666859f2634be47cad505bb5
MSI\soumen <soumen2225@gmail.com>
2018-11-04 17:09:58 +0900

    宿題

--- src/work1/Test.java
+++ src/work1/Test.java
@@ -1,5 +1,11 @@
 package work1;
 
 public class Test {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅３横幅５の長方形の面積は、" + c);
+	}
 
 }

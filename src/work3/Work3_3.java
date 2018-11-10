29d86bcba548f195d9809a3aaa2a9c8fe5ea56cf
MSI\soumen <soumen2225@gmail.com>
2018-11-10 17:04:06 +0900

    宿題

--- src/work3/Work3_3.java
+++ src/work3/Work3_3.java
@@ -3,8 +3,18 @@ package work3;
 public class Work3_3 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int seibetu = 1;
+		int age = 20;
+		System.out.println("こんにちは。");
+		if (seibetu == 0) {
+			System.out.println("私は男です。");
+		} else {
+			System.out.println("私は女です。");
+		}
+		if (seibetu == 0) {
+			System.out.println(age + "歳です。");
+		}
+		System.out.println("よろしくおねがいします。");
 	}
 
 }
--- src/work3/Work3_5.java
+++ src/work3/Work3_5.java
@@ -3,8 +3,23 @@ package work3;
 public class Work3_5 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("[メニュー]1:検索2:登録3:削除4:変更>");
+		int selected = new java.util.Scanner(System.in).nextInt();
+		switch (selected) {
+		case 1:
+			System.out.println("検索します");
+			break;
+		case 2:
+			System.out.println("登録します");
+			break;
+		case 3:
+			System.out.println("削除します");
+			break;
+		case 4:
+			System.out.println("変更します");
+			break;
 
+		}
 	}
 
 }

cd449cba40a5a9021db72ccdaa3410a618dbedef
MSI\soumen <soumen2225@gmail.com>
2018-11-24 00:41:17 +0900

    できませんでした

--- src/work5/homework/Work5_ex.java
+++ src/work5/homework/Work5_ex.java
@@ -13,7 +13,49 @@ public class Work5_ex {
 	 */
 
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 8;
+		int c = 11;
+		int d = 5;
+		calc(a, b);
 
+		System.out.println("aとb");
+		message(calc(a, b));
+		System.out.println("aとc");
+		message(calc(a, c));
+		System.out.println("aとd");
+		message(calc(a, d));
 	}
 
-}
+	public static int calc(int x, int y ) {
+		int ans1 = x + y;
+		return ans1;
+		int ans2 = x - y;
+		return ans2;
+		int ans3 = x * y;
+		return ans3;
+		int ans4 = x / y;
+		return ans4;
+	}
+
+	public static void message(int ans1,ans2,ans3,ans4) {
+		if (ans1 > 0) {
+			System.out.println("計算結果は" + ans1 + "です。");
+		} else {
+			System.out.println("エラー");
+		}
+		if (ans2 > 0) {
+			System.out.println("計算結果は" + ans2 + "です。");
+		} else {
+			System.out.println("エラー");
+		}if (ans3 > 0) {
+			System.out.println("計算結果は" + ans3 + "です。");
+		} else {
+			System.out.println("エラー");
+		}
+		if (ans4 > 0) {
+			System.out.println("計算結果は" + ans4 + "です。");
+		} else {
+			System.out.println("エラー");
+		}
+	}

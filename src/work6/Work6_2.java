c80fd5b026ac581f8c3a4fad95ee86c2d817f534
oono tatsuya home <sideline.sax@gmail.com>
2018-12-16 17:35:15 +0900

    問題1～3

---
+++ src/work6/Work6_2.java
@@ -0,0 +1,10 @@
+package work6;
+
+public class Work6_2 {
+	/*
+	 * 自然数Nがある。
+	 * Nが奇数のとき「odd」、偶数のとき「even」
+	 * と出力して下さい。
+	 */
+
+}
---
+++ src/work6/Work6_3.java
@@ -0,0 +1,9 @@
+package work6;
+
+public class Work6_3 {
+	/*
+	 * 2つの西暦a,bがある。
+	 * aとbが何年間あるか出力してください。
+	 *
+	 */
+}

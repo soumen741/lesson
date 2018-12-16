e288f045dfa8d69cd9ea213341d8446b27bc3bff
MSI\soumen <soumen2225@gmail.com>
2018-12-16 17:58:14 +0900

    6-1,2

--- src/work6/Work6_1.java
+++ src/work6/Work6_1.java
@@ -6,4 +6,16 @@ public class Work6_1 {
 	 * aとbを比較して大きいほうの値を出力してください。
 	 * 同じ値の場合は「eq」と出力して下さい。
 	 */
+	public static void main(String[] args) {
+		int a = (int) (Math.random() * 100);
+		int b = (int) (Math.random() * 100);
+		if (a > b) {
+			System.out.println(a);
+		}
+		if (a < b) {
+			System.out.println(b);
+		} else {
+			System.out.println("eg");
+		}
+	}
 }
--- src/work6/Work6_2.java
+++ src/work6/Work6_2.java
@@ -6,5 +6,12 @@ public class Work6_2 {
 	 * Nが奇数のとき「odd」、偶数のとき「even」
 	 * と出力して下さい。
 	 */
+	public static void main(String[] args) {
+		int N = (int) (Math.random() * 100);
+		if (N % 2 <= 0) {
+			System.out.println("even");
+		} else
+			System.out.println("odd");
+	}
 
 }

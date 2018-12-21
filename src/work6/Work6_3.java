2ddce4ae9f7d9a61c776f88b63b5d26c84b6e3aa
MSI\soumen <soumen2225@gmail.com>
2018-12-21 09:34:46 +0900

    6-3

--- src/work6/Work6_3.java
+++ src/work6/Work6_3.java
@@ -6,4 +6,15 @@ public class Work6_3 {
 	 * aとbが何年間あるか出力してください。
 	 *
 	 */
+	public static void main(String[] args) {
+		System.out.println("西暦aを入力してください");
+		int year1 = new java.util.Scanner(System.in).nextInt();
+		System.out.println("西暦bを入力してください");
+		int year2 = new java.util.Scanner(System.in).nextInt();
+		if (year1 >= year2) {
+			System.out.println(year1 - year2);
+		} else {
+			System.out.println(year2 - year1);
+		}
+	}
 }
